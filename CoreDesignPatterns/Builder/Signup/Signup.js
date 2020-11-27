1134f9324e1d988541b06ca6a8fa523576aad130
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-27 14:54:47 -0400

    Added Builder Pattern

--- CoreDesignPatterns/Builder/Signup/Signup.js
+++ CoreDesignPatterns/Builder/Signup/Signup.js
@@ -2,7 +2,7 @@ const User = require('./User');
 const UserPref = require('./UserPref');
 
 class Signup {
-    async create(name, email, age, photo, nickname, favoriteTopics, isSME, isModerator, isAdmin) {
+    async create({ name, email, age, photo, nickname, favoriteTopics, isSME, isModerator, isAdmin }) {
         const newUser = await new User(name, email, age, photo);
         await new UserPref(newUser, nickname, favoriteTopics, isSME, isModerator, isAdmin);
     }
--- CoreDesignPatterns/Builder/Signup/app.js
+++ CoreDesignPatterns/Builder/Signup/app.js
@@ -1,3 +1,13 @@
-const Signup = require('./Signup');
+// const Signup = require('./Signup');
 
-Signup.create('John Doe', 'ramalaso@gmail.com', 25, 'ramalaso.jpg', 'ramalaso', ['Javascript', 'Node js'], ['Node js'], true, false);
+// Signup.create('John Doe', 'ramalaso@gmail.com', 25, 'ramalaso.jpg', 'ramalaso', ['Javascript', 'Node js'], ['Node js'], true, false);
+
+const SignupBuilder = require('./SignupBuilder');
+
+new SignupBuilder('Raul Laredo', 'ramalaso@gmail.com', 42)
+    .setPhoto('ramalaso.jpg')
+    .setNick('ramalaso')
+    .setTopics(['Node js', 'Javacsript'])
+    .setSME(['Node js'])
+    .setModerator()
+    .create();
